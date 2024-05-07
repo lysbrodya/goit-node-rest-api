189b06e7cee6bee3fe966e55eac67803a373822b
lysbrodya <lysbrodya@gmail.com>
2024-05-08 01:20:49 +0300

    nf

--- helpers/validateId.js
+++ helpers/validateId.js
@@ -4,7 +4,7 @@ import { isValidObjectId } from "mongoose";
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(HttpError(400, `${id} is not valid id`));
+    next(HttpError(404, "Not found"));
   }
   next();
 };
